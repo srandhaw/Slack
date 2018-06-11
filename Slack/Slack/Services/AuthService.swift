e8d5256fefbfeecca328d9a9e80b847bb4ea2636
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-06-11 16:32:32 +0400

    register user web request done

---
+++ Slack/Slack/Services/AuthService.swift
@@ -0,0 +1,69 @@
+//
+//  AuthService.swift
+//  Slack
+//
+//  Created by Sehajbir Randhawa on 6/11/18.
+//  Copyright © 2018 Sehajbir. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService{
+    
+    static let instance = AuthService()
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool {
+        get{
+           return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set{
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToke: String {
+        get{
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set{
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+        
+    }
+    
+    var userEmail: String {
+        get{
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set{
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String,password: String,completion: @escaping CompletionHandler){
+        var email1 = email.lowercased()
+        let header = [
+            "Content-Type":"application/json; charset=utf-8"
+        ]
+        let body: [String: Any] = [
+            "email":email1,
+            "password":password
+        ]
+        
+        Alamofire.request(REGISTER_URL, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString{(response) in
+
+            if(response.result.error==nil){
+                completion(true)
+            }
+            else{
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+    }
+    
+    
+}
--- Slack/Slack/Utilities/Constants.swift
+++ Slack/Slack/Utilities/Constants.swift
@@ -8,7 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool)->()
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+//User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "email"
+
+
+//url constants
+let BASE_URL = "https://slack-clone--.herokuapp.com/v1/"
+let REGISTER_URL = "\(BASE_URL)account/register"
